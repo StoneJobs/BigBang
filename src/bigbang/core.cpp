bef6725235577b91ec97decbb5c60e92cc6ee408
chuanhua.chen <cch809094@163.com>
2020-10-27 17:29:31 +0800

    Comparative study on avoiding the wrong matchmaking (#15)
    * fix match verify tx error
    
    * update

--- src/bigbang/core.cpp
+++ src/bigbang/core.cpp
@@ -61,6 +61,12 @@ static const uint32 REF_VACANT_HEIGHT = 368638;
 #endif
 
 #ifdef BIGBANG_TESTNET
+static const uint32 MATCH_VERIFY_ERROR_HEIGHT = 0;
+#else
+static const uint32 MATCH_VERIFY_ERROR_HEIGHT = 490566;
+#endif
+
+#ifdef BIGBANG_TESTNET
 static const int64 BBCP_TOKEN_INIT = 300000000;
 static const int64 BBCP_BASE_REWARD_TOKEN = 20;
 static const int64 BBCP_INIT_REWARD_TOKEN = 20;
@@ -719,7 +725,7 @@ Errno CCoreProtocol::VerifyBlockTx(const CTransaction& tx, const CTxContxt& txCo
     {
         return DEBUG(ERR_TRANSACTION_INPUT_INVALID, "DeFi relation tx from address must be not equal to sendto address\n");
     }
-    if(tx.nType == CTransaction::TX_DEFI_RELATION && (!CTemplate::IsTxSpendable(tx.sendTo) || !CTemplate::IsTxSpendable(destIn)))
+    if (tx.nType == CTransaction::TX_DEFI_RELATION && (!CTemplate::IsTxSpendable(tx.sendTo) || !CTemplate::IsTxSpendable(destIn)))
     {
         return DEBUG(ERR_TRANSACTION_INVALID, "DeFi tx sendto Address and destIn must be spendable\n");
     }
@@ -822,6 +828,12 @@ Errno CCoreProtocol::VerifyBlockTx(const CTransaction& tx, const CTxContxt& txCo
         throw DEBUG(ERR_TRANSACTION_INPUT_INVALID, "creating fork nAmount must be at least %ld", CTemplateFork::CreatedCoin());
     }
 
+    if (destIn.IsTemplate() && destIn.GetTemplateId().GetType() == TEMPLATE_DEXMATCH
+        && nForkHeight == MATCH_VERIFY_ERROR_HEIGHT)
+    {
+        nForkHeight -= 1;
+    }
+
     if (!destIn.VerifyTxSignature(tx.GetSignatureHash(), tx.nType, tx.hashAnchor, tx.sendTo, vchSig, nForkHeight, fork))
     {
         return DEBUG(ERR_TRANSACTION_SIGNATURE_INVALID, "invalid signature");
@@ -868,7 +880,7 @@ Errno CCoreProtocol::VerifyTransaction(const CTransaction& tx, const vector<CTxO
     {
         return DEBUG(ERR_TRANSACTION_INVALID, "DeFi relation tx from address must be not equal to sendto address\n");
     }
-    if(tx.nType == CTransaction::TX_DEFI_RELATION && (!CTemplate::IsTxSpendable(tx.sendTo) || !CTemplate::IsTxSpendable(destIn)))
+    if (tx.nType == CTransaction::TX_DEFI_RELATION && (!CTemplate::IsTxSpendable(tx.sendTo) || !CTemplate::IsTxSpendable(destIn)))
     {
         return DEBUG(ERR_TRANSACTION_INVALID, "DeFi tx sendto Address and destIn must be spendable\n");
     }
