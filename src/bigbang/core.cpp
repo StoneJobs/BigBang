de6976b64a0b366ff94f4db4bc5a14251e2ae6ba
MathxH Chen <brainfvck@foxmail.com>
2020-11-04 10:41:51 +0800

    Remove 1agwkgwhd address from DeFi Black List (#29)
    * Remove 1agwkgwhd address
    
    * Fix cmpile error

--- src/bigbang/core.cpp
+++ src/bigbang/core.cpp
@@ -141,6 +141,14 @@ static const map<uint256, map<int, set<CDestination>>> mapDeFiBlacklist = {
                     bigbang::CAddress("1agwkgwhdbzhd1hqa5fjvpst42v727befdc6e7a2kv77scr4qapqfhrk1"),
                 },
             },
+            {
+                503704,
+                {
+                    bigbang::CAddress("103vf0z8f5kry0937ar3ac864cbhkfh8efmmy8mxxy27kaq5sf3svbare"),
+                    bigbang::CAddress("1m8sm8bsydnwaabhhfzjgwnxd2rd879g2cnj1nw8d5j3bhv29ftp5z2bs"),
+                    bigbang::CAddress("1r4hh5jnzp5c3pcr92vaqt579b65kpvafx5j8avkn2xq0ksqkdden32g9"),
+                },
+            },
         },
     },
 };
@@ -1380,7 +1388,7 @@ const std::set<CDestination>& CCoreProtocol::GetDeFiBlacklist(const uint256& has
     auto it = mapDeFiBlacklist.find(hashFork);
     if (it != mapDeFiBlacklist.end())
     {
-        for (auto& list : it->second)
+        for (auto& list : boost::adaptors::reverse(it->second))
         {
             if (nHeight >= list.first)
             {
